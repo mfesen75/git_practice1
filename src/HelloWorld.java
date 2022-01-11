50c3607619366169129ca5c1bfeb225342a24c35
Mehmet <fgesen@gmail.com>
2022-01-10 21:09:23 -0500

    work 3 is done and about to be pushed to Github after commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,9 @@ public class HelloWorld {
 
     // doing awesome work 2!
 
+    //doing awesome work 3
+
+    //awesome work 3 is done and ready to be committed
+    // and ready to be pushed to remote repository
 
 }
